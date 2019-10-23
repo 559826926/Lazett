e2fb302bed4f5f9dd3b34ad071c949e5b613962e
559826926 <46727562+559826926@users.noreply.github.com>
2019-10-23 14:14:17 -0500

    Update Lexer.java

--- Lexer.java
+++ Lexer.java
@@ -1,26 +1,53 @@
-/*±≠≠≠±≠±≠±≠±≠±≠±±≠±≠≠±≠±≠±≠±≠±≠±≠±≠±≠±≠≠±≠±≠±≠±≠±≠±≠±≠±≠±≠±≠±±±≠±≠≠≠≠≠±≠±≠±≠±≠±≠±≠±≠±≠±≠±
-Title: Lexer
+/*public class Lexer
+{
 
-Authors: Ethan Jett and Grant Larsen
+  public static void main (String[] arg){
 
-Description: Our futal attempt at creating a Lexer for Lazett
-≠±≠≠≠±≠≠±≠±≠±≠±≠±≠±≠±±≠±≠≠±≠±±≠±±≠±±≠±≠±±≠±±±≠±≠±≠≠±≠±≠±≠±≠±≠±≠±±±≠±≠±≠±±±±≠±≠±±±±≠±≠±≠±≠*/
-import java.util.*;
+    
+    
+    
+    
+  }
+}*/
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * Java program to read file using Scanner class in Java.
+ * java.util.Scanner is added on Java 5 and offer convenient method to read data
+ *
+ * @author
+ */
 public class Lexer {
-
-
-  public static String[] lexing {
-
-  Scanner input = new Scanner(System.in);
-  String purple = input.nextLine();
-  String file = new String(Files.readAllBytes(Paths.get(purple)));
-
-
-
+  
+  public static void main(String args[]) throws FileNotFoundException{
+  reader(); 
+  
+  
+  
+  
   }
 
-
-
-
+    public static String reader()throws FileNotFoundException {
+ 
+        //creating File instance to reference text file in Java
+        File text = new File("text.txt");
+     
+        //Creating Scanner instnace to read File in Java
+        Scanner scnr = new Scanner(text);
+     
+        //Reading each line of file using Scanner class
+        int lineNumber = 1;
+        while(scnr.hasNextLine()){
+            String line = scnr.nextLine();
+            System.out.println("line " + lineNumber + " :" + line);
+            lineNumber++;
+        }      
+   
+    return "";
+    }  
+ 
 }
